3046a9b400e78ecd6a0875dad2c7eff8331959f7
William Lindberg <117780994+william00771@users.noreply.github.com>
2024-12-14 15:32:37 +0100

    add: UpricefileMANUAL model

--- begdelar.api/Models/AppDbContext.cs
+++ begdelar.api/Models/AppDbContext.cs
@@ -5,6 +5,6 @@ namespace begdelar.api.Models
     public class AppDbContext : DbContext
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
-        public DbSet<Product> Products { get; set; }
+        public DbSet<UPricefileManual> UPricefileManuals { get; set; }
     }
 }
--- begdelar.api/Models/UPricefileMANUAL.cs
+++ begdelar.api/Models/UPricefileMANUAL.cs
@@ -1,7 +1,79 @@
 ﻿namespace begdelar.api.Models
 {
-    public class UPricefileMANUAL
-    {
+    using System.ComponentModel.DataAnnotations;
 
+    public class UPricefileManual
+    {
+        [Required, MaxLength(22)]
+        public string Artnr { get; set; }
+
+        [MaxLength(30)]
+        public string? DescrEng { get; set; }
+
+        [MaxLength(15)]
+        public string? Retail { get; set; }
+
+        [MaxLength(15)]
+        public string? Export { get; set; }
+
+        [MaxLength(4)]
+        public string? Prg { get; set; }
+
+        [MaxLength(5)]
+        public string? Disccode { get; set; }
+
+        [MaxLength(30)]
+        public string? DescrLoc { get; set; }
+
+        [MaxLength(3)]
+        public string? Origin { get; set; }
+
+        [MaxLength(10)]
+        public string? Wgt { get; set; }
+
+        [MaxLength(5)]
+        public string? Qnt { get; set; }
+
+        [Required, MaxLength(3)]
+        public string Qtfak1 { get; set; }
+
+        [MaxLength(3)]
+        public string? Qtfak2 { get; set; }
+
+        [Required, MaxLength(4)]
+        public string UnitType { get; set; }
+
+        [MaxLength(22)]
+        public string? ReplacingNo { get; set; }
+
+        [MaxLength(4)]
+        public string? Partgrp { get; set; }
+
+        [MaxLength(1)]
+        public string? StatofOrig { get; set; }
+
+        [MaxLength(2)]
+        public string? ArticleGroup { get; set; }
+
+        [Required, MaxLength(1)]
+        public string Replcode { get; set; }
+
+        [MaxLength(1)]
+        public string? CoreKat { get; set; }
+
+        [MaxLength(1)]
+        public string? CC { get; set; }
+
+        [MaxLength(15)]
+        public string? Custcode { get; set; }
+
+        [MaxLength(10)]
+        public string? Supplno { get; set; }
+
+        [MaxLength(8)]
+        public string? ValidFrom { get; set; }
+
+        [MaxLength(1)]
+        public string? DiscountCode { get; set; }
     }
 }
